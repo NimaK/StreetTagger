0e20413abdd772ab0dc510b023849a506a1e5fe2
Nima <nkhishtan@gmail.com>
2016-08-26 15:43:03 -0400

    code cleanup

--- js/coord-util.js
+++ js/coord-util.js
@@ -7,8 +7,6 @@ function dmsToDecimal(dmsArray, direction) {
     var deg = dmsArray[0][0] / dmsArray[0][1];
     var min = dmsArray[1][0] / dmsArray[1][1];
     var sec = dmsArray[2][0] / dmsArray[2][1];
-    console.log(sec);
-    console.log(sec/3600);
     var degrees = deg + (min / 60) + (sec / 3600);
     if (direction.match(/^(S|W)$/)) {
         degrees = degrees * -1;
